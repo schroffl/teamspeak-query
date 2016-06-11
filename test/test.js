4d98f090221b0a74f132dd3ad54d07b3a7fe042b
Hannes <Hannes.Holzwarth@me.com>
2016-06-11 19:53:48 +0200

    username and password can be bassed as arguments

--- test/test.js
+++ test/test.js
@@ -5,10 +5,10 @@ const TeamspeakQuery = require('../index');
 let test = new TeamspeakQuery('127.0.0.1', 10011);
 
 test.sock.on('connect', () => {
-	test.send('login', 'serveradmin', '123qweasd') // Login 
-		.then(() => test.send('use', { 'sid': 1 })) // Set the virtual server to be used
+	test.send('login', process.argv[2], process.argv[3]) // Login 
+		.then(() => test.send('use', { 'sid': 1 })) // Set the virtual server
 		.then(() => test.send('servernotifyregister', { 'event': 'server' })) // Notify me on any events
-		.then(() => test.send('clientlist', '-uid')) // List all clients and their unique ids
+		.then(() => test.send('clientlist', '-')) // List all clients and their unique ids
 		.then(console.log) // Log the response of the last command
 		.catch(console.error); // Log any errors
 });
