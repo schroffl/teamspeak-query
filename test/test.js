b8573a43f7df7b04779c4cbd5d0e90ffa7739507
Hannes <Hannes.Holzwarth@me.com>
2016-07-01 13:55:31 +0200

    Added some test using mocha and chai

--- test/test.js
+++ test/test.js
@@ -1,14 +1,73 @@
 'use strict';
 
-const TeamspeakQuery = require('../index');
-const query = new TeamspeakQuery(process.argv[4] || '127.0.0.1', process.argv[5] || 10011);
+const expect = require('chai').expect;
+const TeamspeakQuery = require('..');
 
-query.send('login', process.argv[2], process.argv[3])
-	.then(() => query.send('use', 1))
-	.then(() => query.send('servernotifyregister', { 'event': 'server' }))
-	.then(() => console.log('Done! Everything went fine'))
-	.catch(err => console.error('An error occured:', err));
+let testString = 'notifytextmessage targetmode=3 msg=x=y=z invokerid=2 invokername=schroffl invokeruid=3ZLOUe2rnjkorZfn98EdKyadVV4=';
 
-// After teamspeak has processed 'servernotifyregister' we will get notified about any connections
-query.on('cliententerview', data =>
-	console.log(data.client_nickname, 'connected') );
+describe('TeamspeakQuery', () => {
+
+	describe('#escape', () => {
+		it('Should escape strings according to the specs', () => {
+			expect( TeamspeakQuery.escape('\\') ).to.equal('\\\\');
+			expect( TeamspeakQuery.escape('/')  ).to.equal('\\/');
+			expect( TeamspeakQuery.escape(' ')  ).to.equal('\\s');
+			expect( TeamspeakQuery.escape('\n') ).to.equal('\\n');
+			expect( TeamspeakQuery.escape('\r') ).to.equal('\\r');
+			expect( TeamspeakQuery.escape('\t') ).to.equal('\\t');
+			expect( TeamspeakQuery.escape('\v') ).to.equal('\\v');
+			expect( TeamspeakQuery.escape('\f') ).to.equal('\\f');
+		});
+	});
+
+	describe('#unescape', () => {
+		it('Should unescape strings according to the specs', () => {
+			expect( TeamspeakQuery.unescape('\\\\') ).to.equal('\\');
+			expect( TeamspeakQuery.unescape('\\/')  ).to.equal('/');
+			expect( TeamspeakQuery.unescape('\\s')  ).to.equal(' ');
+			expect( TeamspeakQuery.unescape('\\n')  ).to.equal('\n');
+			expect( TeamspeakQuery.unescape('\\r')  ).to.equal('\r');
+			expect( TeamspeakQuery.unescape('\\t')  ).to.equal('\t');
+			expect( TeamspeakQuery.unescape('\\v')  ).to.equal('\v');
+			expect( TeamspeakQuery.unescape('\\f')  ).to.equal('\f');
+		});
+	});
+
+	describe('#assembleCommandString', () => {
+		it('Should correctly construct and escape a command string', () => {
+			expect( TeamspeakQuery.assembleCommandString('command', { 'this': 'is' }, 'just', { 'msg': 'a stupid test' }) )
+			.to.equal('command this=is just msg=a\\sstupid\\stest');
+		});
+	});
+
+	describe('#parse', () => {
+		it('Should detect error- and notify-messages', () => {
+			let notify = TeamspeakQuery.parse('notifytextmessage msg=text'),
+				error = TeamspeakQuery.parse('error id=0 msg=ok');
+
+			expect(notify.type).to.equal('notifytextmessage');
+			expect(notify.params.msg).to.equal('text');
+
+			expect(error.type).to.equal('error');
+			expect(error.params.msg).to.equal('ok');
+		});
+
+		it('Should parse multiple values for the same key into an array', () => {
+			let str = 'clid=1|clid=2',
+				parsed = TeamspeakQuery.parse(str);
+
+			expect(parsed.type).to.equal(null);
+			expect(parsed.params.clid).to.be.a('array');
+		});
+
+		it('Should not fail when an equal sign is in a parameter', () => {
+			let str = 'notifytextmessage msg=x=y=z invokername=This\\s=\\smy\\pname',
+				parsed = TeamspeakQuery.parse(str);
+
+			expect(parsed.type).to.equal('notifytextmessage');
+			expect(parsed.params.msg).to.equal('x=y=z');
+			expect(parsed.params.invokername).to.equal('This = my|name');
+		});
+	});
+
+});
