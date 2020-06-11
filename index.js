b777cb83e2904928c8d59e477f306827a400e221
schroffl <schroffl@users.noreply.github.com>
2020-06-11 17:11:03 +0200

    Add TeamspeakQuery.Custom - See #24

--- index.js
+++ index.js
@@ -3,3 +3,4 @@
 module.exports.Base = require('./lib/base');
 module.exports.Raw = require('./lib/raw');
 module.exports.SSH = require('./lib/ssh');
+module.exports.Custom = require('./lib/custom');
